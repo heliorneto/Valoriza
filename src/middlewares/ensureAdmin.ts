13d5bbad47daffc84bf906b222f316bb5fef70ef
Hélio Neto <helioneto29@hotmail.com>
2021-12-21 20:08:15 -0300

    Verificando se o usuário é administrador

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,9 +1,16 @@
 import { NextFunction, Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
-export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
-    const admin = true;
+export async function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+    const {user_id} = request;
 
-    if(admin){
+    const usersRespositories = getCustomRepository(UsersRepositories);
+
+    const user = await usersRespositories.findOne(user_id);
+    console.log(user)
+
+    if(user?.admin){
         return next();
     }
 
---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayLoad {
+    sub: string;
+}
+
+export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+    const authToken = request.headers.authorization;
+
+    if(!authToken) {
+        return response.status(401).end();
+    }
+
+    const [, token] = authToken.split(" ");
+
+    try{
+        const {sub} = verify(token, "e22ca1d7f12897a46ebd067530601cfc") as IPayLoad;
+        request.user_id = sub;
+        return next();
+    }catch(err){
+        response.status(401).end();
+    }
+}
